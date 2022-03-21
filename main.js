87fa8d406a7e58656019f4e51fde6959098f8ced
Jennifer Jia <jjia2@andrew.cmu.edu>
2022-03-21 01:29:14 -0400

    adding javascript interactions and editing flow of booking process

---
+++ main.js
@@ -0,0 +1,13 @@
+function addVaccineBrand() {
+  if (document.getElementById('pfizer').checked) {
+    vaccine_brand = document.getElementById('pfizer').value;
+  }
+  else if (document.getElementById('moderna').checked) {
+    vaccine_brand = document.getElementById('moderna').value;
+  }
+  else {
+    vaccine_brand = document.getElementById('johnson').value;
+  }
+  console.log(vaccine_brand)
+  document.getElementById('vaccinebrandresults').innerHTML = vaccine_brand;
+}
